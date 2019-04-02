fccab8c08c8efb6b8e7b76e2f8dc3e1ceac3a35a
iandeeph <christ.lupher@gmail.com>
2019-04-03 03:50:31 +0700

    fix bug

--- routes/index.js
+++ routes/index.js
@@ -436,7 +436,7 @@ router.post('/order-in', function(req, res) {
                 cekNamakodePromise.then(function (resRows) {
                     // console.log(resRows);
                     if (!_.isEmpty(resRows) || !_.isUndefined(resRows)) {
-                        total = (parseInt(listStock.jumlah.replace(/[^0-9]/gi, '')) + parseInt(resRows.jumlah));
+                        total = (parseInt(listStock.jumlah.replace(/[^0-9]/gi, '')) + resRows.jumlah);
 
                         queryItemString = "UPDATE item SET " +
                             "hargabeli = '" + hargaBeli + "', " +
